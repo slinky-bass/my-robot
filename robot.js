5a0cf3e8b1f0b1b15196b696e7916c869dd5e239
slinky-bass <slinky_bass@yahoo.co.uk>
2016-09-12 22:18:35 +0200

    modify structure

--- robot.js
+++ robot.js
@@ -1,4 +1,6 @@
 var robot = {
+
+  //MY ROBOT PROPERTIES - WHAT YOUR ROBOT HAS
   colour: 'blue',
   name: 'Engelbert',
   weapons: [
@@ -6,32 +8,67 @@ var robot = {
     'blasters'
   ],
   type: '',
+  vocabulary: [
+  ],
+
+  //MY ROBOT FUNCTIONS - WHAT YOUR ROBOT CAN DO
+  
+  //############### SPEECH RELATED ##############
+  // Make robot speak
   speak: function(words) {
 
-    // Make robot speak
+    
 
   },
-  greeting: function() {
 
-    // Greets user
+  // Greets user
+  greeting: function() {
+ 
     document.getElementById('caption').innerHTML ='<p>' + this.name + ': Hi! My name is ' + this.name +'!</p>';
 
   },
+
+  // Rename your robot
   rename: function(newName){
 
-    // Rename robot
 
   },
+
+  //############### INVENTORY RELATED ##############
+  // Display inventory
+  listItems: function() {
+
+
+  },
+  
+  // Add an item to your robot's inventory
+  addItem: function() {
+
+    
+  },
+
+  // Remove an item to your robot's inventory
+  removeItem: function() {
+
+    
+  },
+
+  // Search for an item to your robot's inventory
+  findItem: function() {
+
+    
+  },
+
+  //############### WEAPONRY RELATED ############
+  // Display weapons
   listWeapons: function() {
 
-    // Display weapons
 
   },
+  
+  // Add a weapon to the robot's arsenal
   addWeapon: function() {
 
-    // Add a weapon to the robot's arsenal
-
+    
   }
 };
-
-robot.greeting();
